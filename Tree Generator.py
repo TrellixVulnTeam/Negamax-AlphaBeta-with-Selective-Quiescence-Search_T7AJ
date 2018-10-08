305fcf228c77353e58c9fe0c90b53097ce9ad37b
SabeerBakir <sabeer.bakir@ucdconnect.ie>
2018-10-08 20:21:28 +0100

    First Commit

---
+++ Tree Generator.py
@@ -0,0 +1,13 @@
+from anytree import Node, RenderTree
+
+udo = Node(1)
+marc = Node(2, parent=udo)
+lian = Node(3, parent=marc)
+dan = Node(4, parent=udo)
+jet = Node(5, parent=dan)
+jan = Node(6, parent=dan)
+joe = Node(7, parent=dan)
+
+for pre, fill, node in RenderTree(udo):
+    print("%s%s" % (pre, node.name))
+aint
